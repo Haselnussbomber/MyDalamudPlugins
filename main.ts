387e73d90ed27f7dc98e60ea403ef04a5c8afbd0
Haselnussbomber <mail@haselnussbomber.de>
2022-04-23 20:00:07 +0200

    initial commit

---
+++ main.ts
@@ -0,0 +1,24 @@
+const user = "Haselnussbomber";
+const repos = [
+  "HaselTweaks",
+];
+
+const output = await Promise.all(repos.map(async (repo) => {
+  const res = await fetch(`https://api.github.com/repos/${user}/${repo}/releases/latest`);
+  const data = await res.json();
+  return {
+    Author: user,
+    Name: repo,
+    InternalName: repo,
+    AssemblyVersion: data.tag_name.replace(/^v/, ""),
+    RepoUrl: `https://github.com/${user}/${repo}`,
+    ApplicableVersion: "any",
+    DalamudApiLevel: 6,
+    DownloadCount: data.assets[0].download_count,
+    LastUpdate: new Date(data.published_at).valueOf() / 1000,
+    DownloadLinkInstall: data.assets[0].browser_download_url,
+    DownloadLinkUpdate: data.assets[0].browser_download_url,
+  };
+}));
+
+await Deno.writeTextFile("repo.json", JSON.stringify(output, null, 2));
