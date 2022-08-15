acf54b218fef57d42e1b26f1bd9f0a9605f90690
Haselnussbomber <mail@haselnussbomber.de>
2022-08-15 06:03:56 +0200

    fetch manifest.json if available

--- main.ts
+++ main.ts
@@ -16,20 +16,30 @@ const clearText = (str) => {
 const output = await Promise.all(repos.map(async (repo) => {
   const res = await fetch(`https://api.github.com/repos/${user}/${repo}/releases/latest`);
   const data = await res.json();
-  return {
-    Author: user,
-    Name: repo,
-    InternalName: repo,
+  const base = {
     AssemblyVersion: data.tag_name.replace(/^v/, ""),
-    RepoUrl: `https://github.com/${user}/${repo}`,
     Changelog: clearText(data.body),
-    ApplicableVersion: "any",
-    DalamudApiLevel: 6,
     DownloadCount: data.assets[0].download_count,
     LastUpdate: new Date(data.published_at).valueOf() / 1000,
     DownloadLinkInstall: data.assets[0].browser_download_url,
     DownloadLinkUpdate: data.assets[0].browser_download_url,
   };
+
+  const manifestAsset = data.assets.find(asset => asset.name == "manifest.json");
+  if (!manifestAsset) {
+    return Object.assign({
+      Author: user,
+      Name: repo,
+      InternalName: repo,
+      RepoUrl: `https://github.com/${user}/${repo}`,
+      ApplicableVersion: "any",
+      DalamudApiLevel: 6,
+    }, base);
+  }
+  
+  const manifestRes = await fetch(manifestAsset.browser_download_url);
+  const manifest = await manifestRes.json();
+  return Object.assign(manifest, base);
 }));
 
 await Deno.writeTextFile("repo.json", JSON.stringify(output, null, 2));
