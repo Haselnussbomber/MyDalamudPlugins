1fe387051069aad13a39d5a2ef99c899d8713f0a
Haselnussbomber <mail@haselnussbomber.de>
2023-03-01 23:57:35 +0100

    support prereleases

--- main.ts
+++ main.ts
@@ -15,32 +15,49 @@ const clearText = (str) => {
 };
 
 const output = await Promise.all(repos.map(async (repo) => {
-  const res = await fetch(`https://api.github.com/repos/${user}/${repo}/releases/latest`);
-  const data = await res.json();
-  const base = {
-    AssemblyVersion: data.tag_name.replace(/^v/, ""),
-    Changelog: clearText(data.body),
-    DownloadCount: data.assets[0].download_count,
-    LastUpdate: new Date(data.published_at).valueOf() / 1000,
-    DownloadLinkInstall: data.assets[0].browser_download_url,
-    DownloadLinkUpdate: data.assets[0].browser_download_url,
+  let res = await fetch(`https://api.github.com/repos/${user}/${repo}/releases/latest`);
+  const latest = await res.json();
+  let out = {
+    AssemblyVersion: latest.tag_name.replace(/^v/, ""),
+    Changelog: clearText(latest.body),
+    DownloadCount: latest.assets[0].download_count,
+    LastUpdate: new Date(latest.published_at).valueOf() / 1000,
+    DownloadLinkInstall: latest.assets[0].browser_download_url,
+    DownloadLinkUpdate: latest.assets[0].browser_download_url,
   };
 
-  const manifestAsset = data.assets.find(asset => asset.name == "manifest.json");
-  if (!manifestAsset) {
-    return Object.assign({
-      Author: user,
-      Name: repo,
-      InternalName: repo,
-      RepoUrl: `https://github.com/${user}/${repo}`,
-      ApplicableVersion: "any",
-      DalamudApiLevel: 6,
-    }, base);
+  let manifest = {
+    Author: user,
+    Name: repo,
+    InternalName: repo,
+    RepoUrl: `https://github.com/${user}/${repo}`,
+    ApplicableVersion: "any",
+    DalamudApiLevel: 6,
+  };
+  const manifestAsset = latest.assets.find(asset => asset.name == "manifest.json");
+  if (manifestAsset) {
+    const manifestRes = await fetch(manifestAsset.browser_download_url);
+    if (manifestRes.ok) {
+      manifest = await manifestRes.json();
+    }
   }
-  
-  const manifestRes = await fetch(manifestAsset.browser_download_url);
-  const manifest = await manifestRes.json();
-  return Object.assign(manifest, base);
+
+  out = Object.assign(manifest, out);
+
+  const prereleaseRes = await fetch(`https://api.github.com/repos/${user}/${repo}/releases/tags/prerelease`);
+  if (prereleaseRes.ok) {
+    const prerelease = await prereleaseRes.json();
+    const prereleaseManifestAsset = prerelease.assets.find(asset => asset.name == "manifest.json");
+    if (prereleaseManifestAsset) {
+      const prereleaseManifestRes = await fetch(prereleaseManifestAsset.browser_download_url);
+      const prereleaseManifest = await prereleaseManifestRes.json();
+
+      out.DownloadLinkTesting = prerelease.assets[0].browser_download_url;
+      out.TestingAssemblyVersion = prereleaseManifest.AssemblyVersion;
+    }
+  }
+
+  return out;
 }));
 
 await Deno.writeTextFile("repo.json", JSON.stringify(output, null, 2));
